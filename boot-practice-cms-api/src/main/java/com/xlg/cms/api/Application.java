4e353e910a56ffcdf521f6e31ee5bef47714fc8b
1367379258 <1367379258@qq.com>
2021-05-28 14:39:07 +0800

    处理信息

--- boot-practice-cms-api/src/main/java/com/xlg/cms/api/Application.java
+++ boot-practice-cms-api/src/main/java/com/xlg/cms/api/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 
 /**
- * @author wangqingwei 2
+ * @author 1367379258
  * Created on 2021-05-28
  */
 @SpringBootApplication(scanBasePackages = "com.xlg")
